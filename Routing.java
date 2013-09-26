3295755b4255386a0f1ac6e0d93fd00d3e81348d
viksingh <vikas.singh.0@gmail.com>
2013-09-26 22:01:02 +1000

    Create Routing.java

---
+++ Routing.java
@@ -0,0 +1,30 @@
+package com.saki.maps;
+
+public class Routing {
+	
+	long id;
+	String filename;
+	String receiver;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	
+	
+
+}
